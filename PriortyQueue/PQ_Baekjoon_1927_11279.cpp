876bae544fb7d7d1c9eb1441c1d5e4cae3d8f364
DESKTOP-13C1M65\ugly2 <gyu_@khu.ac.kr>
2022-08-15 18:48:31 +0900

    0815 silver!

---
+++ PriortyQueue/PQ_Baekjoon_1927_11279.cpp
@@ -0,0 +1,42 @@
+﻿#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int n = 0;
+
+int arr[1000] = { 0 };
+
+int main() {
+	//우선순위 큐
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	//입출력 향상
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	//n을 입력
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		//0이 입력 될 경우 
+		if (x == 0) {
+			if (pq.empty()) {
+				cout << 0 << "\n";
+			}
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x);
+		}
+	}
+
+}
---
+++ Recursion/PaperDivide_Baekjoon_2630.cpp
@@ -0,0 +1,99 @@
+﻿#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int n = 0;
+
+//white paper and blue paper
+int wPaper = 0;
+int bPaper = 0;
+
+
+void getPaperNum(vector < vector<bool>> paper) {
+
+
+	if (paper.size() == 1) {
+		if (paper[0][0] == true) bPaper++;
+		if (paper[0][0] == false) wPaper++;
+	}
+	else {
+		bool isOneColor = true;
+		bool firstColor = paper[0][0];
+
+		//paper이 한 색인지 판단
+		for (vector<bool> bs : paper) {
+			for (bool b : bs) {
+				if (firstColor != b) {
+					isOneColor = false;
+					break;
+				}
+			}
+		}
+		//한 색일 경우
+		if (isOneColor) {
+			if (paper[0][0] == true) bPaper++;
+			if (paper[0][0] == false) wPaper++;
+		}
+		//다른 색 일 경우
+		else {
+			vector<vector<bool>> paperZero;
+			//one해야하는데 오타 ㅎ
+			vector<vector<bool>> paperTwo;
+			vector<vector<bool>> paperThree;
+			vector<vector<bool>> paperFour;
+			//만약 나눠야하면 4개로 나눠서 재귀
+			if (isOneColor == false) {
+				for (int i = 0; i < paper.size() / 2; i++) {
+					paperZero.push_back(vector<bool>());
+					paperTwo.push_back(vector<bool>());
+					paperThree.push_back(vector<bool>());
+					paperFour.push_back(vector<bool>());
+					for (int k = 0; k < paper.size() / 2; k++) {
+						paperZero[i].push_back(paper[i][k]);
+						paperTwo[i].push_back(paper[i][k + paper.size() / 2]);
+						paperThree[i].push_back(paper[i + paper.size() / 2][k]);
+						paperFour[i].push_back(paper[i + paper.size() / 2][k + paper.size() / 2]);
+					}
+				}
+
+				getPaperNum(paperZero);
+				getPaperNum(paperFour);
+				getPaperNum(paperThree);
+				getPaperNum(paperTwo);
+			}
+		}
+	}
+
+}
+
+
+int main() {
+
+	//입출력 향상
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	//n을 입력
+	cin >> n;
+
+	vector<vector<bool>> paper;
+
+	for (int i = 0; i < n; i++) {
+		paper.push_back(vector<bool>());
+		for (int k = 0; k < n; k++) {
+			bool a;
+			cin >> a;
+			paper[i].push_back(a);
+		}
+	}
+
+	//nxn paper 완성
+	getPaperNum(paper);
+		//출력
+	cout << wPaper << "\n" << bPaper;
+}
+
