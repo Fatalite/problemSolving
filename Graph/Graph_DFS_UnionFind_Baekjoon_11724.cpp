caa427dba49487d6a7f3c1ada767b39f646a02e5
DESKTOP-13C1M65\ugly2 <gyu_@khu.ac.kr>
2022-08-16 17:09:54 +0900

    0816

---
+++ Graph/Graph_DFS_UnionFind_Baekjoon_11724.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+//dfs => 시간이 빠르다.
+//union find => 저장메모리가 적다.
+int n = 0;
+int m = 0;
+
+int arr[1009];
+int findRoot(int x) {
+    if (arr[x] == x) {
+        return x;
+    }
+    else {
+        return findRoot(arr[x]);
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    fill_n(arr, 1000, -1);
+
+    for (int i = 0; i < n; i++) {
+        arr[i + 1] = i + 1;
+    }
+
+
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        int u = arr[y];
+        int v = arr[x];
+        for (int k = 1; k <= n; k++) {
+            if (arr[k] == u) {
+                arr[k] = findRoot(v);
+            }
+        }
+
+    }
+    vector<int> tmp;
+    int counts = 0;
+    for (int i = 1; i <= n; i++) {
+        bool isUnique = true;
+        for (int k : tmp) {
+            if (k == arr[i]) {
+                isUnique = false;
+            }
+        }
+        if (isUnique) {
+            tmp.push_back(arr[i]);
+            counts++;
+        }
+    }
+    cout << counts;
+
+}
+
---
+++ Recursion/Z_Baekjoon_1074(important).cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int n = 0;
+int r = 0;
+int c = 0;
+
+int counts = 0;
+
+int main() {
+    cin >> n >> r >> c;
+    //i = 0, 1
+    //n = 3
+    deque<int> tmp;
+    int nt = n - 1;
+    int count = 0;
+    // 3 7 7
+    // n = 3
+    // 16가 경계선
+    // 
+    // 16 = 2^4
+    
+    int nowSize = pow(4, n);
+    while (nowSize != 1) {
+        cout << r << " " << c << " " << endl;
+        nowSize = nowSize / 4;
+        cout << nowSize << endl;
+        if (r >= sqrt(nowSize) && c >= sqrt(nowSize)) {
+            tmp.push_back(4);
+            r = r - sqrt(nowSize);
+            c = c - sqrt(nowSize);
+            continue;
+        }
+        if (r < sqrt(nowSize) && c >= sqrt(nowSize)) {
+            tmp.push_back(2);
+            c = c - sqrt(nowSize);
+            continue;
+        }
+        if (r < sqrt(nowSize) && c < sqrt(nowSize)) {
+            cout << "이게 왜";
+            tmp.push_back(1);
+            continue;
+        }
+        if (r >= sqrt(nowSize) && c < sqrt(nowSize)) {
+            tmp.push_back(3);
+            r = r - sqrt(nowSize);
+            continue;
+        }
+    }
+    // 4 7 7
+    // 1 4 4
+    cout << endl;
+    // 1 * 
+    int a = 1;
+    while (!tmp.empty()) {
+        count = count + (tmp.back() - 1) * a;
+        a = a * 4;
+        //cout << tmp.front() << " " << endl;
+        tmp.pop_back();
+    }
+    cout << endl;
+    cout << count;
+}
+
