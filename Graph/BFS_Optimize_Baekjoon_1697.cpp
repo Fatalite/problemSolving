ef6d5365148c51e953c30c6f4d55bab157da4526
DESKTOP-13C1M65\ugly2 <gyu_@khu.ac.kr>
2022-08-19 14:37:35 +0900

    Create BFS_Optimize_Baekjoon_1697.cpp

---
+++ Graph/BFS_Optimize_Baekjoon_1697.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+using namespace std;
+int n = 0;
+int m = 0;
+
+int timeG = 0;
+
+int main() {
+    
+    cin >> n;
+    cin >> m;
+
+    queue<int> q;
+    vector<bool> checked(100005, false);
+    q.push(n);
+
+    while (!q.empty()) {
+
+        int qs = q.size();
+        
+        for (int i = 0; i < qs; i++) {
+
+           
+
+            if (q.front() == m) {
+                cout << timeG;
+                return 0;
+            }
+
+            if (q.front() < 0 || q.front() > 100001) {
+                q.pop();
+                continue;
+            }
+            else {
+                //m을 넘어간 경우에
+                if (q.front() > m) {
+                    if (checked[q.front()] != true && q.front() < 100001) {
+                        checked[q.front()] = true;
+                        q.push(q.front() - 1);
+                    }
+
+                }
+                else {
+                    if (checked[q.front()] != true && q.front() < 100001) {
+                        checked[q.front()] = true;
+                        q.push(2 * q.front());
+                        q.push(q.front() - 1);
+                        q.push(q.front() + 1);
+                    }
+                }
+                q.pop();
+
+            }
+
+           
+        }
+       
+
+        timeG++;
+    }
+
+}
